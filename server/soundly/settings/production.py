d4c97cef81ced1df6dab07c6930bf0a3a96de45e
muazzeem <muazzem.mamun@gmail.com>
2025-08-05 13:14:06 +0600

    add code

--- server/soundly/settings/production.py
+++ server/soundly/settings/production.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 DEBUG = False
@@ -5,10 +7,15 @@ SECRET_KEY = config("SECRET_KEY")
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://example.com",
+    "https://soundly-beats.vercel.app",
 ]
 
 try:
     from .local import *
 except ImportError:
     pass
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
