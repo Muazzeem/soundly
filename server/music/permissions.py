8e00484bf211afd716aa85c0cb29243c252c2871
Muazzeem <muazzem.mamun@gmail.com>
2025-08-07 12:05:00 +0600

    deploy

--- server/music/permissions.py
+++ server/music/permissions.py
@@ -1,9 +1,5 @@
 from rest_framework import permissions
-from datetime import date
-from django.utils.timezone import localdate
 from music.models import Song
-from users.choices import UserTypeChoice
-from django.conf import settings
 
 class CanUploadSong(permissions.BasePermission):
     """
@@ -11,24 +7,8 @@ class CanUploadSong(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        if not request.user or not request.user.is_authenticated:
-            return False
-
         user = request.user
-        today = localdate()
-        song_count_today = Song.objects.filter(uploader=user, created_at__date=today).count()
-
-        if user.type == UserTypeChoice.BASIC:
-            return song_count_today < settings.SONG_UPLOAD_LIMIT
-        
-        elif user.type == UserTypeChoice.PREMIUM:
-            pass
-
-        elif user.type == UserTypeChoice.ARTIST:
-            pass
-
-        elif user.type == UserTypeChoice.INFLUENCER:
-            pass
-
-        else:
+        if not user or not user.is_authenticated:
             return False
+        temp_song = Song(uploader=user)
+        return temp_song.remaining_uploads > 0
